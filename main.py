71f05503fe00d59ba75fbb060f3a76d9ff7ee927
begly <service.alex@begly.co.uk>
2017-02-13 13:43:17 +0000

    Write event order function

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ class Project():
         self.events = events
         self.starTime = startTime
 
+    def eventOrder(self):
+        ordered = []
+        events = list(self.events)
+
+        # TODO infinite loop if dependencies wrong
+        while len(events) > 0:
+            for event in events:
+                shouldAdd = True
+                for dep in event.dependencies:
+                    if dep not in ordered:
+                        shouldAdd = False
+                        break
+                if shouldAdd:
+                    ordered.append(event)
+                    events.remove(event)
+        return ordered
 
 class Event():
     def __init__(self, name, duration, dependencies):
@@ -15,9 +31,11 @@ class Event():
         self.dependencies = dependencies
 
     def __repr__(self):
-        return "%s - %i - %s" % (self.name, self.duration, self.dependencies)
+        return "%s" % self.name# - %i - %s" % (self.name, self.duration, self.dependencies)
 
 e1 = Event('test1', 2, [])
-e2 = Event('test2', 3, [e1])
-print(e1)
-print(e2)
+e3 = Event('test3',4,[e1])
+e2 = Event('test2', 3, [e1,e3])
+
+p1 = Project([e1,e2,e3])
+print(p1.eventOrder())
