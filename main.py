4c2602a560f96541b3abd651f20e78c4e0c8b709
begly <service.alex@begly.co.uk>
2017-02-13 13:08:15 +0000

    added test event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# this is my computing project prototype
+#!/usr/bin/env python3
 
+# this is my computing project prototype
 
 class Project():
     def __init__(self, events, startTime=0):
@@ -12,4 +13,9 @@ class Event():
         self.name = name
         self.duration = duration
         self.dependencies = dependencies
-    
+
+    def __str__(self):
+        return "%s - %i - %s" % (self.name, self.duration, self.dependencies)
+
+e = Event('test', 2, [])
+print(e)
---
+++ testData.py
@@ -0,0 +1,3 @@
+import main
+
+Event('test', 2, [])
