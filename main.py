5e3a130e5d611db3c97ca86fe95f38fe198b47c9
begly <service.alex@begly.co.uk>
2017-03-09 23:06:31 +0000

    rename

--- main.py
+++ main.py
@@ -1,71 +1,140 @@
 #!/usr/bin/env python3
 
-# this is my computing project prototype
+# Maths critical path analysis
+# https://revisionworld.com/a2-level-level-revision/maths/decision-maths-0/critical-path-analysis
 
-class Project():
-    def __init__(self, events, startTime=0):
-        self.events = events
-        self.starTime = startTime
-
-    # Returns a ordered list of evetns or None if not possible
-    def eventOrder(self):
-        ordered = []
-        events = list(self.events)
-
-        # TODO infinite loop if dependencies wrong
-        while len(events) > 0:
-            itemChanged = False
-            for event in events:
-                shouldAdd = True
-                for dep in event.dependencies:
-                    if dep not in ordered:
-                        shouldAdd = False
-                        break
-                if shouldAdd:
-                    itemChanged = True
-                    ordered.append(event)
-                    events.remove(event)
-
-            if not itemChanged:
-                return None # Events cannot be ordered
-
-        return ordered
-
-    # Updates events earliest & latest start times
-    def calcEarlyTimes(self):
-        ordered = self.eventOrder()
-        for event in ordered:
-            maxEarlyStart = 0
-            for dep in event.dependencies:
-                if dep.earlyStart > maxEarlyStart:
-                    maxEarlyStart = dep.earlyStart
-
-            event.earlyStart = maxEarlyStart
 
 class Event():
-    def __init__(self, name, duration, dependencies, earlyStart=None, lateStart=None):
-        self.name = name
-        self.duration = duration
+    def __init__(self, identifier, dependencies, earlyStart=0, lateStart=0):
+        self.identifier = identifier
         self.dependencies = dependencies
         self.earlyStart = earlyStart
         self.lateStart = lateStart
 
     def __repr__(self):
-        return "%s" % self.name# - %i - %s" % (self.name, self.duration, self.dependencies)
+        return "%s [%i|%i]" % (self.identifier, self.earlyStart, self.lateStart) # - %i - %s" % (self.name, self.duration, self.dependencies)
 
-e1 = Event('e1', 2, [])
-e3 = Event('e3',4,[e1])
-e2 = Event('e2', 3, [e1,e3])
-e4 = Event('e4',4,[e2])
-e5 = Event('e5', 3, [e4])
+class Activity():
+    def __init__(self, name, duration, source=None, target=None):
+        self.name = name
+        self.duration = duration
+        self.source = source
+        self.target = target
 
-events = [e1, e2, e3, e4, e5]
+    def __repr__(self):
+        return "%s (%i)" % (self.name, self.duration)
 
-p1 = Project(events)
-print(p1.eventOrder())
-p1.calcEarlyTimes()
-print(e1.earlyStart)
-print(e2.earlyStart)
-print(e3.earlyStart)
-print(e4.earlyStart)
-print(e5.earlyStart)
+class DummyActivity(Activity):
+    def __init__(self, source, target=None):
+        self.name = 'Dummy'
+        self.duration = 0
+        self.source = source
+        self.target = target
+
+
+class Project():
+    def __init__(self, activites):
+        self.activites = activites
+
+
+
+
+    #     self.eventList = list(eventList)
+    #     self.orderEvents()
+    #
+    # # Returns a list of events in dependency order
+    # #
+    # # For every event which has dependencies in ordered, add this event to ordered
+    # # If no dependencies are in ordered, go to next event
+    # # If list is unorderable, raise an exception
+    # def orderEvents(self):
+    #     ordered = []
+    #     while len(self.eventList) > 0:
+    #         eventAdded = False
+    #         for event in self.eventList:
+    #             addEvent = True
+    #             for dep in event.dependencies:
+    #                 if dep not in ordered:
+    #                     addEvent = False
+    #                     break
+    #             if addEvent:
+    #                 ordered.append(event)
+    #                 self.eventList.remove(event)
+    #                 eventAdded = True
+    #
+    #         if eventAdded == False:
+    #             Exception("Events unorderable")
+    #
+    #     self.eventList = ordered
+    #     return ordered
+    #
+    #
+    # def calcEarlyTime(self):
+    #     for event in self.eventList:
+    #         maxEarlyStart = 0
+    #         for dep in event.dependencies:
+    #             potentialSTime = dep.earlyStart + event.duration
+    #             if potentialSTime > maxEarlyStart:
+    #                 maxEarlyStart = potentialSTime
+    #
+    #         event.earlyStart = maxEarlyStart
+    #
+    # def calcLateTime(self):
+    #     #unsure if this works
+    #     reversedEventList = list(reversed(self.eventList))
+    #     previousEvent = None
+    #     for event in reversedEventList:
+    #
+    #
+    #
+    #         # minLateStart = reverseOrdered[0].duration
+    #         # for dep in event.dependencies:
+    #         #     if dep.duration < minLateStart:
+    #         #         minLateStart = dep.duration
+    #         #
+    #         # event.lateStart = minLateStart
+    #         previousEvent = event
+    #
+    # def calcFloats(self):
+    #     return None
+    #
+    # def criticalActivities(self):
+    #     #Returns a list of the critical activities
+    #     return None
+    #
+    # def criticalPath(self):
+    #     #Returns the order of events in the critical path
+    #     return None
+    #
+    # def workerEstimate(self):
+    #     #estimate the number of workers required to complete the task in a given
+    #     # timeframe
+    #     return None
+    #
+    # def createSchedule(self):
+    #     return None
+
+
+activites = {
+    'A': Activity('A', 3),
+    'B': Activity('B', 5),
+    'C': Activity('C', 2),
+    'D': Activity('D', 3),
+    'E': Activity('E', 3),
+    'F': Activity('F', 5),
+    'G': Activity('G', 1),
+    'H': Activity('H', 2),
+}
+
+event_0 = Event(0, [])
+event_1 = Event(1, [ activites['A'] ])
+event_2 = Event(2, [ activites['B'], activites['D'] ])
+event_3 = Event(3, [ activites['C'] ])
+
+dummy_34 = DummyActivity(event_3)
+
+event_4 = Event(4, [ activites['E'], dummy_34])
+event_5 = Event(5, [ activites['F'],  activites['G'] ])
+event_6 = Event(6, [ activites['H'] ])
+
+P1 = Project([event_0, event_1, event_2, event_3, event_4, event_5, event_6])
